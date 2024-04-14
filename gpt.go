0c5662a1ed949c09c838d1ea07cde67ccf6863a5
awen <zhiwen_der@qq.com>
2024-04-14 12:13:42 +0800

    完成gpt 会话创建和查询

--- gpt.go
+++ gpt.go
@@ -1,4 +1,12 @@
 package model
 
-type Conversation struct {
+type AppChatConversationItem struct {
+	Id         string `column:"id" json:"id"`
+	Picture    string `column:"picture" json:"picture"`
+	UserId     string `column:"user_id" json:"userId"`
+	Title      string `column:"title" json:"title"`
+	LastModel  string `column:"last_model" json:"lastModel"`
+	LastMsg    string `column:"last_msg" json:"lastMsg"`
+	LastTime   string `column:"last_time" json:"lastTime"`
+	CreateTime string `column:"create_time" json:"createTime"`
 }
