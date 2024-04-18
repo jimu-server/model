e736b93a4a8c81c0cb55bfba25adea250b9dcf99
awen <zhiwen_der@qq.com>
2024-04-18 13:48:52 +0800

    添加 ollama 模型 结构体

--- gpt.go
+++ gpt.go
@@ -10,3 +10,23 @@ type AppChatConversationItem struct {
 	LastTime   string `column:"last_time" json:"lastTime"`
 	CreateTime string `column:"create_time" json:"createTime"`
 }
+
+type AppChatMessage struct {
+	Id             string `column:"id" json:"id"`
+	ConversationId string `column:"conversation_id" json:"conversationId"`
+	UserId         string `column:"user_id" json:"userId"`
+	Role           string `column:"role" json:"role"`
+	Content        string `column:"content" json:"content"`
+	CreateTime     string `column:"create_time" json:"createTime"`
+}
+
+type LLmModel struct {
+	Id         string `column:"id" json:"id"`
+	Name       string `column:"name" json:"name"`
+	Model      string `column:"model" json:"model"`
+	Icon       string `column:"icon" json:"icon"`
+	Parameters string `column:"parameters" json:"parameters"`
+	Size       string `column:"size" json:"size"`
+	Download   string `column:"download" json:"download"`
+	CreateTime string `column:"create_time" json:"createTime"`
+}
