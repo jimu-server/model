21d81eb8022370d59a9be04249e84e9e6b9ee740
awen <zhiwen_der@qq.com>
2024-05-11 16:45:03 +0800

    添加配置接口

--- setting.go
+++ setting.go
@@ -3,8 +3,10 @@ package model
 type AppSetting struct {
 	Id         string `column:"id" json:"id"`
 	Pid        string `column:"pid" json:"pid"`
+	UserId     string `column:"user_id" json:"user_id"`
 	Name       string `column:"name" json:"name"`
 	Value      string `column:"value" json:"value"`
+	ToolId     string `column:"tool_id" json:"tool_id"`
 	Setting    string `column:"setting" json:"setting"`
 	CreateTime string `column:"create_time" json:"create_time"`
 }
